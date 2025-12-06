1022f21b1db1243f820a4f0a0c850094c7a39894
HuyenDang <huyendtb22dccn400@gmail.com>
2025-12-06 12:44:28 +0700

    Chỉnh sửa API log

--- Source Code/backend/controllers/log.controller.js
+++ Source Code/backend/controllers/log.controller.js
@@ -5,10 +5,9 @@ const BiometricData = require("../models/BiometricData");
 const Device = require("../models/device.model");
 const mqttClient = require("../config/mqtt");
 
-//[GET] http://localhost:3000/log - Lấy ra danh sách log truy cập
+//[GET] http://localhost:3000/log/:deviceId - Lấy ra danh sách log truy cập theo thiết bị
 module.exports.getAllAccessLogs = async (req, res) => {
   try {
-    const manager = req.user; // Lấy từ token decode
     const { deviceId } = req.params;
 
     if (!deviceId) {
@@ -18,28 +17,11 @@ module.exports.getAllAccessLogs = async (req, res) => {
       });
     }
 
-    // 1. Lấy các user con
-    const childUsers = await User.find({
-      parent_id: manager.id,
-      role: "user",
-    }).select("_id");
-
-    const childUserIds = childUsers.map((u) => u._id.toString());
-
-    // 2. Danh sách user hợp lệ
-    const allowedUserIds = [manager.id, ...childUserIds];
-
-    // 3. Lấy log theo device + (user hợp lệ hoặc user null)
     const logs = await AccessLog.find({
       device_id: deviceId,
-      $or: [
-        { user_id: { $in: allowedUserIds } }, // log của manager và user con
-        { user_id: null }, // log thất bại (không có user)
-      ],
     })
       .sort({ createdAt: -1 })
-      .populate("user_id", "fullName email role")
-      .populate("device_id", "name location");
+      .populate("user_id", "fullName email role");
 
     return res.status(200).json({
       success: true,
